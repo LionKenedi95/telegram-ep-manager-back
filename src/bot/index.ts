ece6719cdd7005f54628b01dc5bca0a9967084ce
sergey.murashow <sergey.murashow@gmail.com>
2024-12-09 21:46:40 +0400

    Start bot dev

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,11 +1,27 @@
 import { Telegraf } from 'telegraf'
 import { config } from 'dotenv'
+import { newUser } from '../db/functions/newUser.db'
+import { getUser } from '../db/functions/getUser.db'
 
 config()
 
 const bot = new Telegraf('7952621195:AAEVLGCwBaYwwdszrp2nHw5mFvMCE0mTWAE')
 
-bot.start(ctx => ctx.reply('Welcome!'))
-bot.help(ctx => ctx.reply('Send me a sticker'))
-bot.on('my_chat_member', ctx => ctx.reply('👍'))
 bot.launch()
+bot.start(ctx => {
+	const { id, first_name, last_name, username } = ctx.from
+	newUser(id, first_name, last_name, username)
+	ctx.reply('Welcome to the bot!')
+})
+
+bot.on('my_chat_member', ctx => {
+	const { id, first_name, last_name, username } = ctx.from
+	getUser(id).then(user => {
+		if (!user.businesses.length) {
+			ctx.reply('You have no businesses yet. Create one!')
+		}
+	})
+})
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
